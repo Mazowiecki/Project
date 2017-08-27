51e4dc8357e6af81fa192c8e907f56bf182e1761
Stanisław Mazowiecki <Stmazowiecki@gmail.com>
2017-08-27 19:22:22 +0200

    working on RWD (finished header, navigtion[added hamburger], support)

--- js/script.jsx
+++ js/script.jsx
@@ -4,9 +4,33 @@ $(document).ready(function(){
     $(".slider_container").slick({
         infinite: true,
         responsive: true,
-        // autoplay: true,
-        // autoplaySpeed: 5000,
+        autoplay: true,
+        autoplaySpeed: 5000,
         arrows: false,
         dots: true,
     });
+
+
+$(".cross").hide();
+$(".menu").hide();
+
+
+$(".hamburger").click(function() {
+    $(".menu").slideToggle( "slow", function() {
+        $(".hamburger").hide();
+        $(".cross").show();
+    });
+});
+
+$(".cross").click(function() {
+    $(".menu").slideToggle( "slow", function() {
+        $(".cross").hide();
+        $(".hamburger").show();
+    });
+});
+
+
+
+
+
 });
